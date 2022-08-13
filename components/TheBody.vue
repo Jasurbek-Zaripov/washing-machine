e8afe8244ec890cd358d9d86951c885fe7911b83
Jasurbek-Zaripov <zaripovjasurbek2001@gmail.com>
2022-08-13 23:22:23 +0500

    fixed responsive

--- components/TheBody.vue
+++ components/TheBody.vue
@@ -2,14 +2,31 @@
     <div class="container mt-5">
         <h1 class="text-uppercase text-center">{{ data.title }}</h1>
         <div class="row mt-5">
-            <div class="col d-flex align-items-center justify-content-center" v-for="info of data.items"
-                :key="info.img">
-                <div class="card mb-5 border-color" style="max-width: 18rem; min-height: 155px;min-width: 350px;">
-                    <div class="card-header"> </div>
+            <div
+                class="col d-flex align-items-center justify-content-center"
+                v-for="info of data.items"
+                :key="info.img"
+            >
+                <div
+                    class="card mb-5 border-color"
+                    style="
+                        max-width: 18rem;
+                        min-height: 155px;
+                        min-width: 350px;
+                    "
+                >
+                    <div class="card-header"></div>
                     <div class="card-body">
                         <h5 class="card-title">
-                            <div class="col d-flex justify-content-start align-items-start">
-                                <img class="logo me-2" :src="info.img" style="height: 25px;" alt="">
+                            <div
+                                class="col d-flex justify-content-start align-items-start"
+                            >
+                                <img
+                                    class="logo me-2"
+                                    :src="info.img"
+                                    style="height: 25px"
+                                    alt=""
+                                />
                                 <h5>{{ info.title }}</h5>
                             </div>
                         </h5>
@@ -19,20 +36,39 @@
             </div>
         </div>
         <div class="mt-5">
-            <hr>
+            <hr />
             <div class="my-5">
                 <h3 class="text-center">{{ data.item }}</h3>
             </div>
             <div class="row">
-                <div class="col mb-5" v-for="(problem, index) of data.problems" :key="index">
-                    <div class="card shadow" style="width: 18rem;">
-                        <img :src="problem.img" style="width: 286px;height: 203px;" class="card-img-top" alt="...">
+                <div
+                    class="col mb-5"
+                    v-for="(problem, index) of data.problems"
+                    :key="index"
+                >
+                    <div class="card shadow" style="width: 18rem">
+                        <img
+                            :src="problem.img"
+                            style="width: 286px; height: 203px"
+                            class="card-img-top"
+                            alt="..."
+                        />
                         <div class="card-body">
-                            <h5 class="card-title" style="height: 3rem; overflow-y: hidden;">{{ problem.title }}</h5>
+                            <h5
+                                class="card-title"
+                                style="height: 3rem; overflow-y: hidden"
+                            >
+                                {{ problem.title }}
+                            </h5>
                             <p class="card-text card-text-dinamic">
                                 {{ problem.body }}
                             </p>
-                            <TheButton style="margin: 2rem auto 0.6rem auto;width: fit-content;" />
+                            <TheButton
+                                style="
+                                    margin: 2rem auto 0.6rem auto;
+                                    width: fit-content;
+                                "
+                            />
                         </div>
                     </div>
                 </div>
@@ -42,60 +78,107 @@
         <div>
             <h3 class="text-center">{{ data.price.title }}</h3>
             <div class="row">
-                <div class="col bb d-flex align-items-center justify-content-between px-5 py-3 m-2 text-nowrap"
-                    v-for="(price, idx) of data.price.items" :key="idx">
-                    <span class="d-inline-block"><b>{{ price.name }}</b></span>
+                <div
+                    class="col bb d-flex align-items-center justify-content-between px-5 py-3 m-2 text-nowrap"
+                    v-for="(price, idx) of data.price.items"
+                    :key="idx"
+                >
+                    <span class="d-inline-block"
+                        ><b>{{ price.name }}</b></span
+                    >
                     <span class="d-inline-block ps-3">{{ price.price }}</span>
                 </div>
             </div>
         </div>
         <div class="mt-5">
             <h3 class="text-center mb-5">{{ data.steps.title }}</h3>
-            <div class="row">
-                <div class="col-xl-5 shadow m-3 step-body" v-for="(step, idx) of data.steps.items" :key="idx">
+            <div class="row w-100">
+                <div
+                    class="col-xl-5 shadow m-3 step-body"
+                    v-for="(step, idx) of data.steps.items"
+                    :key="idx"
+                >
                     <div class="w-50 p-3">
                         <h1>{{ '0' + (idx + 1) }}</h1>
                         <h3 class="text-uppercase">{{ step.name }}</h3>
                         <p>{{ step.body }}</p>
                     </div>
-                    <div class="step-img" :style="'background-image: url(' + step.img + ');'"></div>
+                    <div
+                        class="step-img"
+                        :style="'background-image: url(' + step.img + ');'"
+                    ></div>
                 </div>
             </div>
         </div>
         <div class="mt-5">
             <h3 class="text-center">{{ data.logo.title }}</h3>
-            <div class="d-flex align-items-center justify-content-center" style="flex-wrap: wrap;">
-                <img v-for="logo of data.logo.items" class="m-2 p-2 shadow"
-                    style="height: 50px; background-color: white; border:2px solid white;border-radius: 5px;"
-                    :key="logo" :src="logo" alt="">
+            <div
+                class="d-flex align-items-center justify-content-center"
+                style="flex-wrap: wrap"
+            >
+                <img
+                    v-for="logo of data.logo.items"
+                    class="m-2 p-2 shadow"
+                    style="
+                        height: 50px;
+                        background-color: white;
+                        border: 2px solid white;
+                        border-radius: 5px;
+                    "
+                    :key="logo"
+                    :src="logo"
+                    alt=""
+                />
             </div>
             <div id="contact"></div>
         </div>
-        <div class="mt-5 row" style="flex-direction: column-reverse;">
+        <div class="mt-5 row" style="flex-direction: column-reverse">
             <div class="col google-maps mt-3">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191884.74521379903!2d69.13894024105231!3d41.282770138343835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b0cc379e9c3%3A0xa5a9323b4aa5cb98!2z0KLQsNGI0LrQtdC90YIsINCj0LfQsdC10LrQuNGB0YLQsNC9!5e0!3m2!1sru!2s!4v1660300508041!5m2!1sru!2s"
-                    width="100%" height="100%" style="border:0; border-radius: 5px; min-height: 25rem;"
-                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    width="100%"
+                    height="100%"
+                    style="border: 0; border-radius: 5px; min-height: 25rem"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
             </div>
             <div class="col">
-                <div class="d-flex align-items-center justify-content-center mb-2">
-                    <img :src="data.info.img" style="height: 30px;" alt="" class="logo me-2">
-                    <h3 class="text-uppercase m-0">{{ data.info.title }}</h3>
+                <div
+                    class="d-flex align-items-center justify-content-center mb-2"
+                >
+                    <img
+                        :src="data.info.img"
+                        style="height: 30px"
+                        alt=""
+                        class="logo me-2"
+                    />
+                    <h3 class="text-uppercase m-0 about">
+                        {{ data.info.title }}
+                    </h3>
                 </div>
                 <p>{{ data.info.body1 }}</p>
-                <p><b>{{ data.info.body2 }}</b></p>
-                <p><b>{{ data.info.body3 }}</b></p>
-                <h4><b><u>{{ data.info.body4 }}</u></b></h4>
+                <p>
+                    <b>{{ data.info.body2 }}</b>
+                </p>
+                <p>
+                    <b>{{ data.info.body3 }}</b>
+                </p>
+                <h4>
+                    <b
+                        ><u>{{ data.info.body4 }}</u></b
+                    >
+                </h4>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { store, event, lang } from "@/store/store";
+import { store, event, lang } from '@/store/store';
 let mode = ref(true);
-event.on('mode', (data) => {
+event.on('mode', data => {
     mode.value = data;
 });
 let data = ref({});
--- components/TheNavigate.vue
+++ components/TheNavigate.vue
@@ -1,36 +1,63 @@
 <template>
     <div class="head" :class="mode && 'light'">
         <header class="row shadow px-3 mb-3">
-            <div class="col d-flex justify-content-start align-items-end" id="display-none">
-                <img class="ms-5" src="/company/orange.png" style="height: 45px;">
+            <div
+                class="col d-flex justify-content-start align-items-end"
+                id="display-none"
+            >
+                <img
+                    class="ms-5"
+                    src="/company/orange.png"
+                    style="height: 45px"
+                />
             </div>
-            <div class="col d-flex justify-content-start align-items-center">
+            <div
+                class="col d-flex justify-content-start align-items-center navigator-nav"
+            >
                 <nav>
                     <ul>
                         <li class="text-nowrap">
-                            <a :class="mode && 'light-color'" :data-text="'&nbsp;' + data.menu.home" href="#">&nbsp;{{
-                                    data.menu.home
-                            }}&nbsp;</a>
+                            <a
+                                :class="mode && 'light-color'"
+                                class="text-opacity"
+                                :data-text="'&nbsp;' + data.menu.home"
+                                href="#"
+                                >&nbsp;{{ data.menu.home }}&nbsp;</a
+                            >
                         </li>
                         <li class="text-nowrap">
-                            <a :class="mode && 'light-color'" :data-text="'&nbsp;' + data.menu.about"
-                                href="#about">&nbsp;{{ data.menu.about
-                                }}&nbsp;</a>
+                            <a
+                                :class="mode && 'light-color'"
+                                class="text-opacity"
+                                :data-text="'&nbsp;' + data.menu.about"
+                                href="#about"
+                                >&nbsp;{{ data.menu.about }}&nbsp;</a
+                            >
                         </li>
                         <li class="text-nowrap">
-                            <a :class="mode && 'light-color'" :data-text="'&nbsp;' + data.menu.contact"
-                                href="#contact">&nbsp;{{ data.menu.contact
-                                }}&nbsp;</a>
+                            <a
+                                :class="mode && 'light-color'"
+                                class="text-opacity"
+                                :data-text="'&nbsp;' + data.menu.contact"
+                                href="#contact"
+                                >&nbsp;{{ data.menu.contact }}&nbsp;</a
+                            >
                         </li>
                     </ul>
                 </nav>
             </div>
             <div class="col d-flex align-items-center justify-content-center">
-                <a class="coll" href="tel://+998999891414"><img class="logo" height="25px" style="height: 25px;"
-                        src="/icons/call.svg" alt=""></a>
+                <a class="coll" href="tel://+998999891414"
+                    ><img
+                        class="logo"
+                        height="25px"
+                        style="height: 25px"
+                        src="/icons/call.svg"
+                        alt=""
+                /></a>
                 <h3 class="h1 mx-3 text-nowrap">+998 99 989 14 14</h3>
-                <div class="ms-2" style="cursor: pointer;" @click="change()">
-                    <img height="27px" :src="data.img" alt="">
+                <div class="ms-2" style="cursor: pointer" @click="change()">
+                    <img height="27px" :src="data.img" alt="" />
                 </div>
                 <div>
                     <TheMode />
@@ -45,25 +72,27 @@ import { lang, store, event } from '@/store/store';
 let mode = ref(true);
 useHead({
     bodyAttrs: {
-        class: mode.value ? 'light' : ''
-    }
+        class: mode.value ? 'light' : '',
+    },
 });
-event.on('mode', (data) => {
+event.on('mode', data => {
     mode.value = data;
     useHead({
         bodyAttrs: {
-            class: data ? 'light' : ''
-        }
+            class: data ? 'light' : '',
+        },
     });
 });
 let data = ref({
     menu: store.get('ru/menu'),
-    img: store.get('ru/lang')
+    img: store.get('ru/lang'),
 });
 function change() {
     lang.now = lang.now == 'ru' ? 'uz' : 'ru';
-    data.value.img = lang.now == 'ru' ? store.get('ru/lang') : store.get('uz/lang');
-    data.value.menu = lang.now == 'ru' ? store.get('ru/menu') : store.get('uz/menu');
+    data.value.img =
+        lang.now == 'ru' ? store.get('ru/lang') : store.get('uz/lang');
+    data.value.menu =
+        lang.now == 'ru' ? store.get('ru/menu') : store.get('uz/menu');
     event.emit('lang');
 }
 </script>
@@ -103,5 +132,11 @@ function change() {
 .logo {
     margin: 0;
     padding: 5px;
+}
+.text-opacity {
+    transition: 1s !important;
+}
+.text-opacity:hover {
+    color: transparent !important;
 }
 </style>
